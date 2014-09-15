da755235040629146b7b6a729b03089dd3bfbdc1
Arthike <arturoandrade@gmail.com>
2014-09-15 16:53:36 -0600

    First Version
    Function has not worked (but neither the vector version presented as an
    example)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a list with the functions to manipulate a matrix() (get and set) and its inverse (getSol and setSol)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        Sol <- NULL
+        set <- function(y) {
+                x <<- y
+                Sol <<- NULL
+        }
+        get <- function() x
+        setSol <- function(solve) Sol <<- solve
+        getSol <- function() Sol
+        list(set = set, get = get,
+             setSol = setSol,
+             getSol = getSol)
 }
 
 
-## Write a short comment describing this function
+## This function caches the inverse of a Matrix x. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        Sol <- x$getSol()
+        if(!is.null(Sol)) {
+                message("getting cached data")
+                return(Sol)
+        }
+        data <- x$get()
+        Sol <- solve(data, ...)
+        x$setSol(Sol)
+        Sol ## Return a matrix that is the inverse of 'x'
 }
